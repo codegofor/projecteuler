471a5f798c23671fc4a7923cd813382a639fc774
James Yerkes <codegofor@gmail.com>
2016-05-25 17:48:05 -0400

    changed to func expression within Println

--- problem1.go
+++ problem1.go
@@ -8,15 +8,15 @@ package main
 
 import "fmt"
 
-var sum int
-
 func main() {
-    for i := 0; i < 1000; i++ {
-        if i % 3 == 0 || i % 5 == 0 {
-            sum += i
-        }
-    }
-
-    fmt.Println(sum)
+	// Using a func expression just to be cool
+	fmt.Println(func() int {
+		var sum int
+		for i := 0; i < 1000; i++ {
+			if i%3 == 0 || i%5 == 0 {
+				sum += i
+			}
+		}
+		return sum
+	}())
 }
-
